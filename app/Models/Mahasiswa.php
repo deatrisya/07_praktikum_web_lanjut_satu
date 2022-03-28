e93338f693809ee21fa8b67395c6c2cd3dcf6de8
deatrisya <mirelldee47@gmail.com>
2022-03-28 11:47:17 +0700

    Add fillable to mahasiswa model

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmai;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Mahasiswa extends Model
+
+class Mahasiswa extends Model //Definisi model
 {
+    protected $table ="mahasiswas"; //Eloquent akan membuat model mahasiswa
+    public $timestamps = false;
+    protected $primaryKey ='nim'; //Memanggil isi DB dengan primarykey
+
+    protected $fillable = [
+        'nim',
+        'nama',
+        'kelas',
+        'jurusan',
+        'no_handphone',
+    ];
     use HasFactory;
 }
