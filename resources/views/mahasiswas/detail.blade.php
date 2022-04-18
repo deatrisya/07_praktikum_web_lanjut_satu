8c1c5976172df7a259f898119f3c07157fa51fc7
deatrisya <mirelldee47@gmail.com>
2022-04-18 20:59:35 +0700

    Menambahkan kolom foto pada halaman detail

--- resources/views/mahasiswas/detail.blade.php
+++ resources/views/mahasiswas/detail.blade.php
@@ -11,6 +11,7 @@
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item"><b>Nim: </b>{{$Mahasiswa->nim}}</li>
                     <li class="list-group-item"><b>Nama: </b>{{$Mahasiswa->nama}}</li>
+                    <li class="list-group-item"><b>Foto: </b><img width= "100px" height="100px" src="{{asset('storage/'.$Mahasiswa->foto)}}" alt="foto-profil"></li>
                     <li class="list-group-item"><b>Kelas: </b>{{$Mahasiswa->kelas->nama_kelas}}</li>
                     <li class="list-group-item"><b>Jurusan: </b>{{$Mahasiswa->jurusan}}</li>
                     <li class="list-group-item"><b>No Handphone: </b>{{$Mahasiswa->no_handphone}}</li>
